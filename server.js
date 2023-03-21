ef9caf339cbf421a69caf765bf14a602545dbab3
PDochev <plamendochev0@gmail.com>
2023-03-21 11:15:16 +0000

    routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const { urlencoded } = require("body-parser");
+
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require("mongoose"),
+  Task = reequire("./api/models/todoListModel"),
+  bodyParser = require("body-parser");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/Tododb");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require("./api/routes/todoListRoutes");
+routes(app);
+
+app.listen(port);
+
+console.log("todo list RESTful API server started on: " + port);
